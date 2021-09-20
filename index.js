c2695c88deb324513bf84f6e1180425cc4813210
Radu Baltag <baltag.radu@gmail.com>
2021-09-20 21:31:31 +0200

    rendering every animal's page

--- index.js
+++ index.js
@@ -1,25 +1,35 @@
 const express = require("express")
+const pets = require("./pets")
 const app = express()
 const port = process.env.PORT || 5000
-
-function parameters(req, res) {
-    res.json({
-        kitchen: req.params.kitchen,
-        dish: req.params.dish
-    })
-}
 
 app.get('/', (req, res) => res.send(`
     <h1>Adopt a Pet!</h1>
     <p>Browse through the links below to find your new furry friend:</p>
     <ul>
-        <li>Dogs</li>
-        <li>Cats</li>
-        <li>Rabbits</li>
+        <li><a href="/animals/dogs">Dogs</a></li>
+        <li><a href="/animals/cats">Cats</a></li>
+        <li><a href="/animals/rabbits">Rabbits</a></li>
     </ul>
-    <h1>List of pets</h1>
 `));
 
-app.get('/cookbook/:kitchen/:dish', parameters)
+app.get('/animals/:pet_type', (req, res) => {
+    let petItem = ""
+    pets[req.params.pet_type].forEach(pet => petItem += `<li><a href="/animals/${req.params.pet_type}/${pets[req.params.pet_type].indexOf(pet)}">${pet.name}</a></li>`)
+    return res.send(`
+        <h1>List of ${req.params.pet_type}</h1>
+        <ul>${petItem}</ul>
+`)})
+
+app.get('/animals/:pet_type/:pet_id', (req, res) => {
+    let pet = pets[req.params.pet_type][req.params.pet_id]
+    return res.send(`
+        <h1>${pet.name}</h1>
+        <p>Age: ${pet.age}</p>
+        <p>Breed: ${pet.breed}</p>
+        <p>${pet.description}</p>
+        <img src="${pet.url}" alt="">
+    `)}
+)
 
 app.listen(port, () => console.log(`Server listening at http://localhost:${port}`));
---
+++ pets.js
@@ -0,0 +1,38 @@
+const pets = {
+    'dogs': [
+        {
+            'name': 'Spot',
+            'age': 2,
+            'breed': 'Dalmatian',
+            'description': 'Spot is an energetic puppy who seeks fun and adventure!',
+            'url': '../../images/dalmatian.jpeg'
+        },
+        {
+            'name': 'Shadow',
+            'age': 4,
+            'breed': 'Border Collie',
+            'description': 'Eager and curious, Shadow enjoys company and can always be found tagging along at your heels!',
+            'url': '../../images/collie.jpeg'
+        }
+    ],
+    'cats': [
+        {
+            'name': 'Snowflake',
+            'age': 1,
+            'breed': 'Tabby',
+            'description': 'Snowflake is a playful kitten who loves roaming the house and exploring.',
+            'url': '../../images/tabby.jpeg'
+        }
+    ],
+    'rabbits': [
+        {
+            'name': 'Easter',
+            'age': 4,
+            'breed': 'Mini Rex',
+            'description': 'Easter is a sweet, gentle rabbit who likes spending most of the day sleeping.',
+            'url': '../../images/bunny.png'
+        }
+    ]
+ }
+  
+ module.exports = pets;
